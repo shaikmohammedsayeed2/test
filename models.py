94b5cafecfacaf8bcdb387220c926a66cbefcb5a
Srivardhan <54314801+psrivardhan@users.noreply.github.com>
2023-04-24 17:16:12 +0530

    Update models.py

--- models.py
+++ models.py
@@ -7,11 +7,11 @@ from database import Base
 class Binary(Base):
     id = Column(Integer, primary_key=True, index=True)
 
+    blob_storage = Column(String)
     blob_size = Column(Integer)
     is_active = Column(Boolean)
     created_by = Column(Integer, ForeignKey("TBL_PERSON.id")) 
     created_at = Column(DateTime)
-    blob_storage = Column(String)
 
     __tablename__ = 'TBL_BINARY'
 
@@ -21,11 +21,11 @@ class Conference(Base):
 
     conf_title = Column(String)
     description = Column(String)
-    is_active = Column(Boolean)
-    created_at = Column(DateTime)
     conf_binary_id = Column(Integer, ForeignKey("TBL_BINARY.id"))
     lab_id = Column(Integer, ForeignKey("TBL_LAB.id"))
+    is_active = Column(Boolean)
     created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    created_at = Column(DateTime)
 
     __tablename__ = 'TBL_CONFERENCE'
 
@@ -37,8 +37,8 @@ class ContactUs(Base):
     email = Column(String)
     phone = Column(String)
     is_active = Column(Boolean)
-    created_at = Column(DateTime)
     created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    created_at = Column(DateTime)
 
     __tablename__ = 'TBL_CONTACT_US'
 
@@ -47,12 +47,12 @@ class Lab(Base):
     id = Column(Integer, primary_key=True, index=True)
 
     name = Column(String)
-    overview = Column(String)
-    is_active = Column(Boolean)
-    created_at = Column(DateTime)
     lab_logo_id = Column(Integer, ForeignKey("TBL_BINARY.id"))
+    overview = Column(String)
     contact_id = Column(Integer, ForeignKey("TBL_CONTACT_US.id"))
+    is_active = Column(Boolean)
     created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    created_at = Column(DateTime)
 
     __tablename__ = 'TBL_LAB'
 
@@ -78,7 +78,7 @@ class Person(Base):
     personal_web_url = Column(String)
     is_active = Column(Boolean)
     created_at = Column(DateTime)
-    profile_binary = Column(Integer, ForeignKey("TBL_BINARY.id"))
+    profile_binary_id = Column(Integer, ForeignKey("TBL_BINARY.id"))
     created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
 
     labs = relationship("LabMember")
@@ -89,8 +89,8 @@ class Person(Base):
 class PersonRole(Base):
     id = Column(Integer, primary_key=True, index=True)
 
-    is_active = Column(Boolean)
     person_role_name = Column(String)  # This field type is a guess.
+    is_active = Column(Boolean)
 
     __tablename__ = 'TBL_PERSON_ROLE'
 
@@ -100,10 +100,11 @@ class Publication(Base):
 
     pub_title = Column(String)
     description = Column(String)
-    created_at = Column(DateTime)
     pub_binary_id = Column(Integer, ForeignKey("TBL_BINARY.id"))
     lab_id = Column(Integer, ForeignKey("TBL_LAB.id"))
     created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    created_at = Column(DateTime)
+    type = Column(String)
 
     __tablename__ = 'TBL_PUBLICATION'
 
@@ -111,8 +112,8 @@ class Publication(Base):
 class Role(Base):
     id = Column(Integer, primary_key=True, index=True)
 
-    is_active = Column(Boolean)
     role_name = Column(String)  
+    is_active = Column(Boolean)
 
     __tablename__ = 'TBL_ROLE'
 
@@ -120,11 +121,51 @@ class Role(Base):
 class Slider(Base):
     id = Column(Integer, primary_key=True, index=True)
 
-    is_active = Column(Boolean)
-    created_at = Column(DateTime)
     slider_binary_id = Column(Integer, ForeignKey("TBL_BINARY.id"))
     lab_id = Column(Integer, ForeignKey("TBL_LAB.id"))
+    is_active = Column(Boolean)
     created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    created_at = Column(DateTime)
 
     __tablename__ = 'TBL_SLIDER'
 
+
+class Events(Base):
+    id = Column(Integer, primary_key=True, index=True)
+
+    lab_id = Column(Integer, ForeignKey("TBL_LAB.id"))
+    title = Column(String)
+    description = Column(String)
+    binary_id = Column(Integer, ForeignKey("TBL_BINARY.id"))
+    created_at = Column(DateTime) 
+    created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    is_active = Column(Boolean)
+
+    __tablename__ = 'TBL_EVENTS'
+
+
+class Patent(Base):
+    id = Column(Integer, primary_key=True, index=True)
+
+    publication_id = Column(Integer, ForeignKey("TBL_PUBLICATION.id"))
+    description = Column(String)
+    lab_id = Column(Integer, ForeignKey("TBL_LAB.id"))
+    created_at = Column(DateTime) 
+    created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    is_active = Column(Boolean)
+
+    __tablename__ = 'TBL_PATENT'
+
+
+class PosterDemo(Base):
+    id = Column(Integer, primary_key=True, index=True)
+
+    lab_id = Column(Integer, ForeignKey("TBL_LAB.id"))
+    binary_id = Column(Integer, ForeignKey("TBL_BINARY.id"))
+    description = Column(String)
+    created_at = Column(DateTime) 
+    created_by = Column(Integer, ForeignKey("TBL_PERSON.id"))
+    is_active = Column(Boolean)
+    type = Column(String)
+
+    __tablename__ = 'TBL_POSTER_DEMO'
