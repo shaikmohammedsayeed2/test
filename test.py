1f4dde2ec6e05639fac6f3915f4cfa58204b6773
Nikhi0916 <es19btech11030@iith.ac.in>
2023-04-27 23:04:45 +0530

    removed blob size

--- test.py
+++ test.py
@@ -178,7 +178,7 @@ async def insert_into_binary_table(db:Session, url:str):
     bin_entry = models.Binary(
         blob_storage = url,
         is_active = True,
-        blob_size = -1,
+        #blob_size = -1,
         #created_by = 1      ##TODO: Insert logeed in perosn id
     )
     db.add(bin_entry)
