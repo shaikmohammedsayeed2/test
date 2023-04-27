93f0a93bdb2156fb2680ae8c77e34c7538c16a16
Nikhi0916 <es19btech11030@iith.ac.in>
2023-04-27 12:01:06 +0530

    created sql queries to get publications and conferences

---
+++ sql/conference.sql
@@ -0,0 +1,16 @@
+/* This gives us the list of conferences present*/
+SELECT conf_title,description,blob_storage
+FROM 
+	(
+		(public."TBL_CONFERENCE" as CO INNER JOIN public."TBL_BINARY" as bin ON bin.id = CO.conf_binary_id)
+    )
+		
+WHERE CO.lab_id = {0}
+UNION
+SELECT pub_title,description,blob_storage
+FROM
+    (
+        (public."TBL_PUBLICATION" as PB INNER JOIN public."TBL_BINARY" as bin ON bin.id = PB.pub_binary_id) 
+    )
+
+WHERE PB.lab_id = {0} AND (PB.type LIKE '%conference%');
---
+++ sql/publications.sql
@@ -0,0 +1,8 @@
+/* This gives us the list of publications present*/
+SELECT pub_title,description,blob_storage
+FROM 
+	(
+		(public."TBL_PUBLICATION" as PB INNER JOIN public."TBL_BINARY" as bin ON bin.id = PB.pub_binary_id)
+    )
+		
+WHERE PB.lab_id = {0} AND (PB.type LIKE '%journal%');
