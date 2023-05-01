0097deb32b54402580452070dc8b57b2dc2e6f48
Nikhilesh0916 <100563638+Nikhilesh0916@users.noreply.github.com>
2023-05-01 16:06:43 +0530

    made change to upload file api

--- api/other.py
+++ api/other.py
@@ -12,17 +12,33 @@ STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=rlestora
 IMAGE_CONTAINER = "rleassests"
 
 
+#@router.post("/uploadfile/")
+#async def create_upload_file(file: UploadFile = File(...)):
+#    blob_service_client = BlobServiceClient.from_connection_string(STORAGE_CONNECTION_STRING)
+#    container_client = blob_service_client.get_container_client(IMAGE_CONTAINER)
+#    blob_client = container_client.get_blob_client(file.filename)
+#    data = await file.read()
+#    blob_client.upload_blob(data,overwrite=True)
+#    return {
+#        "url":blob_client.url,
+#        "filename": file.filename
+#        }
+    
 @router.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
+async def create_upload_file(files: list[UploadFile] = File(...)):
     blob_service_client = BlobServiceClient.from_connection_string(STORAGE_CONNECTION_STRING)
     container_client = blob_service_client.get_container_client(IMAGE_CONTAINER)
-    blob_client = container_client.get_blob_client(file.filename)
-    data = await file.read()
-    blob_client.upload_blob(data,overwrite=True)
-    return {
-        "url":blob_client.url,
-        "filename": file.filename
-        }
+    response = dict()
+    for fi in files:
+        blob_client = container_client.get_blob_client(fi.filename)
+        data = await fi.read()
+        blob_client.upload_blob(data,overwrite=True)
+        response[fi.filename] = blob_client.url
+        #return {
+        #    "url":blob_client.url,
+        #    "filename": fi.filename
+        #    }
+    return response        
 
 
 @router.post("/feedback")
