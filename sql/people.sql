b6105d3379b1ef6790c8ce4c616ff6d4440324a9
Nikhilesh0916 <100563638+Nikhilesh0916@users.noreply.github.com>
2023-05-01 15:38:34 +0530

    -

--- sql/people.sql
+++ sql/people.sql
@@ -1,11 +1,12 @@
 /*Returns the details of the lab members given lab id as argument*/
-SELECT name,roll_number,LM.id,person_role_name as role_name,linkedin_url,github_url,personal_web_url,blob_storage as profile_url 
+SELECT name,roll_number,LM.id,person_role_name as user_role_name,linkedin_url,github_url,personal_web_url,blob_storage as profile_url ,role_name as user_access
 FROM 
-	((
+	(((
 		(public."TBL_LAB_MEMBER" as LM INNER JOIN public."TBL_PERSON" as person ON LM.person_id = person.id) 
 	 						INNER JOIN 
 	 	public."TBL_BINARY" as bin ON bin.id = person.profile_binary_id
-	) INNER JOIN public."TBL_PERSON_ROLE" as prole ON prole.id = lm.person_role_id)
-	
+	) INNER JOIN public."TBL_PERSON_ROLE" as prole ON prole.id = lm.person_role_id) INNER JOIN
+	public."TBL_ROLE" as urole on urole.id = lm.role_id
+	)
 		
 WHERE LM.lab_id = {0} AND person.is_active;
