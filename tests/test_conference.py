412d0145a1e8792583e7a02e9b05f1315b65f1eb
Nikhilesh0916 <100563638+Nikhilesh0916@users.noreply.github.com>
2023-05-01 23:27:39 +0530

    adding access levels

--- tests/test_conference.py
+++ tests/test_conference.py
@@ -2,7 +2,7 @@ import models
 from tests.base import *
 from fastapi.encoders import jsonable_encoder
 
-def create_new_conference(lab_id:int):
+def create_new_conference(lab_id:int,auth_cookie):
     conf_data = {
         "conf_title": "Test_conf",
         "description": "Conf_Description",
@@ -11,6 +11,8 @@ def create_new_conference(lab_id:int):
         "end_date": "2023-04-29",
         "conf_pdf": "string"
     }
+    
+    client.cookies.set(COOKIE_KEY, auth_cookie)
 
     response = client.post(
         "/conference",
@@ -29,7 +31,7 @@ def test_get_conference():
     
     lab_id = lab_response.json()
     
-    conf_response = create_new_conference(lab_id)
+    conf_response = create_new_conference(lab_id,authorized_jwt_token_admin)
     
     # Check for status code
     assert conf_response.status_code == 200
@@ -49,7 +51,7 @@ def test_create_conference():
     
     lab_id = lab_response.json()
     
-    conf_response = create_new_conference(lab_id)
+    conf_response = create_new_conference(lab_id,authorized_jwt_token_admin)
     
     # Check for status code
     assert conf_response.status_code == 200
@@ -70,7 +72,7 @@ def test_update_conference():
     
     lab_id = lab_response.json()
     
-    conf_response = create_new_conference(lab_id)
+    conf_response = create_new_conference(lab_id,authorized_jwt_token_admin)
     
     # Check for status code
     assert conf_response.status_code == 200
@@ -83,6 +85,10 @@ def test_update_conference():
         conference = mock_db.query(models.Conference).filter(models.Conference.id == conference_id).first()
         conference.conf_title = "Updated name"
         conference.description = "Updated description"
+        conference.lab_id = lab_id
+        conference.start_date = "2023-05-12"
+        conference.end_date = "2023-05-17"
+        conference.conf_binary_id = conference.conf_binary_id
         
         # Update using the Update API
         response = client.put(
@@ -95,6 +101,10 @@ def test_update_conference():
         
         assert conference.conf_title == updated_conf.conf_title
         assert conference.description == updated_conf.description
+        assert conference.lab_id == updated_conf.lab_id
+        assert conference.start_date == updated_conf.start_date
+        assert conference.end_date == updated_conf.end_date
+        assert conference.conf_binary_id == updated_conf.conf_binary_id
     
     # Test is done
 
@@ -107,7 +117,7 @@ def test_delete_conference():
     
     lab_id = lab_response.json()
     
-    conf_response = create_new_conference(lab_id)
+    conf_response = create_new_conference(lab_id,authorized_jwt_token_admin)
     
     # Check for status code
     assert conf_response.status_code == 200
@@ -127,4 +137,16 @@ def test_delete_conference():
     with TestingSessionLocal() as mock_db:
         conference = mock_db.query(models.Conference).filter(models.Conference.id == conference_id).first()
         assert conference is None        
-            
+
+def test_create_conference_access_levels():
+    lab_response = create_new_lab()
+    
+    # Check for status code
+    assert lab_response.status_code == 200
+    
+    lab_id = lab_response.json()
+    
+    # Create a new conference Unauthorized
+    conf_response = create_new_conference(lab_id,unauthorized_jwt_token)
+    assert conf_response.status_code == 200
+                
