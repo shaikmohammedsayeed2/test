4ceefc59ac51f53cf5ee2a23b4d43ca6c6f42fb9
Srivardhan <54314801+psrivardhan@users.noreply.github.com>
2023-05-05 23:32:53 +0530

    Small modifications

--- database.py
+++ database.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://shaikmohammedsayeed2:dEjBXls6bHg4@ep-throbbing-heart-402486.us-east-2.aws.neon.tech/rlm"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+Base.metadata.create_all(bind=engine)
